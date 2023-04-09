b99c603e0c3902ceb9c4f1ffc574d17e09a56b1d
kevin-wairi <kevinwairi@gmail.com>
2023-04-09 19:04:57 +0300

    Updated logIn in user.js

--- js/user.js
+++ js/user.js
@@ -1,16 +1,22 @@
 document.addEventListener('DOMContentLoaded',()=>{})
 let start = document.getElementById('starter')
-start.addEventListener('click',loadFordInput)
 let logIn = document.getElementById('divForm')
+let logInForm = document.getElementById('logIn')
+let btnSubmit = document.getElementById('btnSubmit')
 
 
 
-function loadFordInput(){
-    logIn.style.visibility='visible';
+// unhides the form DIV
+start.addEventListener('click',()=>{logIn.style.visibility='visible';})
 
-}
-
-
-
+//the X that closes the form DIV
 let close = document.getElementById('close')
 close.addEventListener('click',()=>{ logIn.style.visibility='hidden';})
+
+btnSubmit.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    let userName = document.getElementById('user').value
+    let userLocation = document.getElementById('userlocation').value
+
+})
